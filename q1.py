48b46191bd29adc4d23d95a74fb64b873c3bd962
camila <camilailiberg@gmail.com>
2022-09-22 21:35:58 -0400

    no working

--- q1.py
+++ q1.py
@@ -1,3 +1,16 @@
 import hashlib
 
 #hashlib.sha256(word.encode("ascii", "ignore")).hexdigest()
+
+find = '69d8c7575198a63bc8d97306e80c26e04015a9afdb92a699adaaac0b51570de7'
+
+path = '/Users/camilailigaray/Library/CloudStorage/OneDrive-FloridaInternationalUniversity/Blockchain_CIS4731/Assignment_1/words.txt'
+
+with open (path,'r') as  opened_file:
+    words = opened_file.read()
+    for word in words:
+        
+        if hashlib.sha256(word.encode("ascii","ignore")).hexdigest() == find:
+            print(word)
+
+# print(words)
